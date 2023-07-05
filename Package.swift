6b5da89749805060eb36380d69b843a3e8e026d5
MMQ <sadiq.siddiqui@hotmail.com>
2023-07-05 10:12:26 +0500

    Fixing Package.swift issue

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 
 let package = Package(name: "SJFluidSegmentedControl",
                       products: [.library(name: "SJFluidSegmentedControl",
-                                          targets: ["SJFluidSegmentedControl_Example"])],
-                      targets: [.target(name: "SJFluidSegmentedControl_Example2",
+                                          targets: ["SJFluidSegmentedControl_Example2"])],
+                      targets: [.target(name: "SJFluidSegmentedControl_Example",
                                         exclude: ["Info.plist"]),
                                 ],
                       swiftLanguageVersions: [.v5])
