dd4bc3d3ed4381e296efc3e4eef40446e0faae82
MMQ <sadiq.siddiqui@hotmail.com>
2023-07-05 09:36:33 +0500

    Swift Package Support added

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(name: "SJFluidSegmentedControl",
+                      platforms: [.macOS(.v10_13),
+                                  .iOS(.v11),
+                                  .tvOS(.v11),
+                                  .watchOS(.v4)],
+                      products: [.library(name: "SJFluidSegmentedControl",
+                                          targets: ["SJFluidSegmentedControl"])],
+                      targets: [.target(name: "SJFluidSegmentedControl_Example",
+                                        path: "Source",
+                                        exclude: ["Info.plist"],
+                                        linkerSettings: [.linkedFramework("CFNetwork",
+                                                                          .when(platforms: [.iOS,
+                                                                                            .macOS,
+                                                                                            .tvOS,
+                                                                                            .watchOS]))]),
+                                .testTarget(name: "SJFluidSegmentedControl_Tests",
+                                            dependencies: ["SJFluidSegmentedControl"],
+                                            path: "Tests",
+                                            exclude: ["Info.plist", "Test Plans"],
+                                            resources: [.process("Resources")])],
+                      swiftLanguageVersions: [.v5])
